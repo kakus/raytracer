f0f010a1f25b708591bfbbbb54972674512e2f43
Kamil Misiowiec <mkakus@gmail.com>
2019-05-19 23:14:55 +0200

    raytracing done in more than one weekend

--- app.js
+++ app.js
@@ -4605,12 +4605,19 @@ class qu_texture {
         gl.texParameteri(3553 /* TEXTURE_2D */, gl.TEXTURE_MIN_FILTER, filter);
         gl.texParameteri(3553 /* TEXTURE_2D */, gl.TEXTURE_WRAP_S, wrap);
         gl.texParameteri(3553 /* TEXTURE_2D */, gl.TEXTURE_WRAP_T, wrap);
-        if (data) {
-            gl.texImage2D(3553 /* TEXTURE_2D */, 0, format, format, type, data);
+        let typed_data = null;
+        if (data instanceof Array) {
+            typed_data = type === 5121 /* UNSIGNED_BYTE */ ? new Uint8Array(data) : new Float32Array(data);
         }
         else {
-            gl.texImage2D(3553 /* TEXTURE_2D */, 0, format, width, height, 0, format, type, data);
+            typed_data = data;
         }
+        gl.texImage2D(3553 /* TEXTURE_2D */, 0, format, width, height, 0, format, type, typed_data);
+    }
+    destroy(gl) {
+        qu_assert(this.id != undefined);
+        gl.deleteTexture(this.id);
+        this.id = undefined;
     }
     bind(unit = 0) {
         this.gl.activeTexture(33984 /* TEXTURE0 */ + unit);
@@ -4723,6 +4730,7 @@ class qu_attribute {
         //         array.childs.splice(idx, 0, this);
         //     }
         // }
+        this.on_value_change_event.bind(e => qu_attribute.on_any_attribute_change.broadcast(e));
         if (qu_attribute.on_new_attribute_delegate) {
             setTimeout(() => qu_attribute.on_new_attribute_delegate(this), 0);
         }
@@ -4762,6 +4770,7 @@ class qu_attribute {
 // all attributes, also arrays
 qu_attribute.g_list = [];
 qu_attribute.g_array_list = [];
+qu_attribute.on_any_attribute_change = new qu_multicast_event();
 class qu_array_attribute extends qu_attribute {
     constructor(value_, outer, element_default_ctor) {
         super(value_, outer);
@@ -4823,7 +4832,7 @@ class qu_debug_explorer {
         widget.classList.add('debug-attribute');
         if (parent) {
             let non_array_attr = parent.nextElementSibling;
-            while (non_array_attr.classList.contains('array-element')) {
+            while (non_array_attr && non_array_attr.classList.contains('array-element')) {
                 non_array_attr = non_array_attr.nextElementSibling;
             }
             this.root.insertBefore(widget, non_array_attr);
@@ -4921,6 +4930,9 @@ class qr_webgl_viewport {
         this.gl = this.canvas.getContext('webgl');
         // this.gl.enable(egl.DEPTH_TEST);
     }
+    enable_extension(id) {
+        qu_assert(this.gl.getExtension(id));
+    }
     make_shader(vertex, fragment) {
         let gl = this.gl;
         let compile = (type, source) => {
@@ -4946,17 +4958,61 @@ class qr_webgl_viewport {
     }
 }
 class qu_sphere {
-    constructor([x, y, z] = [0, 0, 0], in_radious = 1.) {
+    constructor([x, y, z] = [Math.random() * 10, 0, Math.random() * 10], in_radious = Math.random() * 0.5 + 0.1, type = 0, [r, g, b] = [0.5, 0.5, 0.5]) {
         this.pos = new qu_attribute(vec3.create(), this);
         this.radius = new qu_attribute(1., this);
+        this.mat_type = new qu_attribute(0, this);
+        this.mat_attu = new qu_attribute(vec3.fromValues(.5, .5, .5), this);
+        this.mat_fuzz = new qu_attribute(0, this);
+        this.mat_ni = new qu_attribute(0, this);
         this.pos.set_value(vec3.fromValues(x, y, z));
         this.radius.set_value(in_radious);
+        this.mat_type.value = type;
+        this.mat_attu.value = vec3.fromValues(r, g, b);
+    }
+    to_array() {
+        let data = [];
+        for (let key in this) {
+            let prop = this[key];
+            if (prop instanceof qu_attribute) {
+                if (typeof prop.value === 'number') {
+                    data.push(prop.value, 0, 0, 1);
+                }
+                else {
+                    data.push.apply(data, prop.value);
+                    data.push(1);
+                }
+            }
+        }
+        return new Float32Array(data);
+    }
+}
+class qu_asset {
+    constructor(_path) {
+        this._path = _path;
+        this._is_loaded = false;
+    }
+    async_load() {
+        if (this.is_loaded()) {
+            return Promise.resolve(this.data);
+        }
+        return fetch(this._path)
+            .then(res => res.text())
+            .then(txt => {
+            this.data = txt;
+            this._is_loaded = true;
+            return txt;
+        });
+    }
+    is_loaded() {
+        return this._is_loaded;
     }
 }
 let debug_widget = new qu_debug_explorer();
 class qc_app {
     constructor() {
         this.quad = qr_webgl_mesh.make_quad();
+        this.raytracer_frag_src = new qu_asset(`/raytracer_frag.glsl`);
         this.mouse_down = false;
         this.last_touch_pos = [0, 0];
         this.key_down = {};
@@ -4964,21 +5020,37 @@ class qc_app {
         this.do_update = new qu_attribute(true, this);
         this.rays_per_pixel = new qu_attribute(1, this);
         this.fov = new qu_attribute(45, this);
-        this.cam_position = new qu_attribute(vec3.fromValues(0, 0, 1), this);
+        this.cam_position = new qu_attribute(vec3.fromValues(0, 0, 5), this);
         this.cam_rotation = new qu_attribute(vec3.create(), this);
         this.cam_sens = new qu_attribute(.1, this);
         this.cam_lens = new qu_attribute(0., this);
         this.cam_focus_dist = new qu_attribute(1., this);
         this.cam_orbit = new qu_attribute(false, this);
         this.frame_idx = new qu_attribute(0, this);
+        this.spheres = new qu_array_attribute([
+            new qu_sphere([0, -100, 0], 99, 0, [.8, .8, 0]),
+            new qu_sphere([0, 0, 0], 1., 2, [1, 1, 1]),
+            new qu_sphere([2.1, 0, 0], 1., 0, [.1, 0.2, 0.5]),
+            new qu_sphere([-2.1, 0, 0], 1., 1, [0.8, 0.6, .2])
+        ], this, qu_sphere);
         this.app_start_time = Date.now();
         this.cam_matrix = mat4.create();
         this.cam_quat = quat.create();
         this.flip = false;
     }
-    //spheres = new qu_array_attribute([new qu_sphere()], this, qu_sphere);
+    load() {
+        let promises = [];
+        for (let prop in this) {
+            let obj = this[prop];
+            if (obj instanceof qu_asset) {
+                promises.push(obj.async_load());
+            }
+        }
+        Promise.all(promises).then(_ => this.init());
+    }
     init() {
         this.webgl_viewport = new qr_webgl_viewport('#canvas');
+        this.webgl_viewport.enable_extension('OES_texture_float');
         // document.body.appendChild(this.canvas.canvas);
         this.texture_shader = this.webgl_viewport.make_shader(`
             attribute vec3 vertex;
@@ -4996,248 +5068,17 @@ class qc_app {
         // use texture from register 0
         this.texture_shader.set_uniformi('tex', 0);
         this.raytracer_shader = this.webgl_viewport.make_shader(`
-            uniform vec2 u_viewport_size;
+            uniform mediump vec2 u_viewport_size;
             attribute vec3 vertex;
             varying vec4 position;
             void main() {
                 vec2 ratio = u_viewport_size / u_viewport_size.x;
                 position = vec4(vertex, 1) * vec4(ratio, 1, 1);
                 gl_Position = vec4(vertex, 1.);
-            }`, 
-        /* Fragment */
-        `
-            #define FLT_MAX 3.402e+38
-            #define PI 3.14159265359
-
-            precision highp float;
-
-            varying vec4 position;
-
-            uniform float u_frame;
-            uniform float u_time;
-            uniform float u_rays_per_pixel;
-            uniform vec2  u_viewport_size;
-            uniform float u_fov;
-            uniform mat4  u_view;
-            uniform float u_lens;
-
-            uniform sampler2D u_prev;
-
-            float g_rand_idx = 1.;
-            float rand() {
-                const float a = 12.9898, b = 78.233, c = 43758.5453;
-                float dt = dot( fract(gl_FragCoord.xy * (g_rand_idx += .001) + (u_time/1000.)), vec2( a, b ) ), 
-                // highp float dt = dot( gl_FragCoord.xy, vec2( a, b ) ), 
-                            sn = mod( dt, PI );
-                return fract(sin(sn) * c);
-            }
-
-            vec2 randv2(float length) {
-                return vec2(rand(), rand()) * 2. * length - length;
-            }
-            vec3 randv3(float length) {
-                return vec3(rand(), rand(), rand()) * 2. * length - length;
-            }
-
-            vec3 rand_point_in_sphere() {
-                // vec3 r = randv3(1.);
-                // return normalize(r) * pow(rand(), 1./3.);
-                return normalize(randv3(1.));
-                // return randv3(.5);
-            }
-
-            vec2 rand_point_on_circle() {
-                float alpha = rand() * PI;
-                return vec2(sin(alpha), cos(alpha));
-            }
-
-            struct ray { vec3 ori; vec3 dir; };
-
-            vec3 ray_point(const ray r, float t) {
-                return r.ori + t * r.dir;
-            }
-
-            struct tmaterial {
-                int  type;
-                vec3 albedo;
-                float fuz; // for metal
-            };
-
-            struct hit_result {
-                float t; //time
-                vec3  p; //point
-                vec3  n; //normal
-                tmaterial m;
-            };
-
-            struct sphere {
-                vec3 center; 
-                float radius;
-                tmaterial material;
-            };
-
-            bool sphere_hit(const sphere self, const ray r, float tmin, float tmax, out hit_result hit) {
-                vec3 oc = r.ori - self.center;
-                float a = dot(r.dir, r.dir);
-                float b = 2. * dot(oc, r.dir);
-                float c = dot(oc, oc) - self.radius * self.radius;
-                float det = b*b - 4.*a*c; 
-
-                bool b_hit = false;
-                if (det > 0.) {
-                    float tmp = (-b - sqrt(det))/(2.*a);
-
-                    b_hit = tmp > tmin && tmp < tmax || 
-                           (tmp = (-b + sqrt(det)) / (2.*a), tmp > tmin && tmp < tmax);
-
-                    if (b_hit) {
-                        hit.t = tmp;
-                        hit.p = ray_point(r, tmp);
-                        hit.n = (hit.p - self.center) / self.radius;
-                        hit.m = self.material;
-                    }
-                }
-                return b_hit;
-            }
-
-            #define SPHERES_NUM 4
-            sphere spheres[SPHERES_NUM];
-
-            bool hit_spheres(const ray r, out hit_result res) {
-                hit_result hit;
-                bool b_hit = false;
-                float t_min = 0.001;
-                float t_max = FLT_MAX;
-
-                for (int i = 0; i < SPHERES_NUM; ++i) {
-                    if (sphere_hit(spheres[i], r, t_min, t_max, hit)) {
-                        t_max = hit.t;
-                        res   = hit;
-                        b_hit = true;
-                    }
-                }
-
-                return b_hit;
-            }
-
-            float schlick(float cosine, float ref_idx) {
-                float r0 = (1. - ref_idx) / (1. + ref_idx);
-                r0 = r0 * r0;
-                return r0 + (1. - r0) * pow(1. - cosine, 5.);
-            }
-
-            vec3 scatter(inout ray r, const hit_result hit) {
-                // lambertian
-                if (hit.m.type == 0) {
-                    vec3 target = hit.p + hit.n + rand_point_in_sphere();
-                    r = ray(hit.p, target - hit.p);
-                    return hit.m.albedo;
-                }
-                // metal
-                else if (hit.m.type == 1) {
-                    vec3 t = reflect(normalize(r.dir), hit.n);
-                    r = ray(hit.p, t + rand_point_in_sphere() * hit.m.fuz);
-                    return hit.m.albedo;
-                }
-                // diaelectric (glass)
-                else if (hit.m.type == 2) {
-                    vec3 n = hit.n;
-                    const float ref_idx = 1.45;
-                    float ni = 1. / ref_idx;
-                    float hit_dot = dot(r.dir, hit.n);
-                    float cosine = -hit_dot / length(r.dir);
-
-                    if (hit_dot > 0.) {
-                        n = -n;
-                        ni = 1. / ni;
-                        cosine *= -ref_idx;
-                    }
-
-                    vec3 refracted = refract(r.dir, n, ni);
-                    if (dot(refracted, refracted) != 0.) {
-                        if (rand() > schlick(cosine, ref_idx)) {
-                            r = ray(hit.p, refracted);
-                            return hit.m.albedo;
-                        }
-                    }
-
-                    r = ray(hit.p, reflect(normalize(r.dir), hit.n));
-                    return hit.m.albedo;
-                }
-                return vec3(0);
-            }
-
-            bool my_refract(inout vec3 v, const vec3 n, const float ni_over_nt) {
-                vec3 uv = normalize(v);
-                float dt = dot(uv, n);
-                float discriminant = 1. - ni_over_nt * ni_over_nt * (1. - dt * dt);
-                if (discriminant > 0.) {
-                    v = ni_over_nt * (uv - n * dt) - n * sqrt(discriminant);
-                    return true;
-                }
-                return false;
-            }
-
-            vec3 color(ray r) {
-                hit_result hit;
-                vec3 attenuation = vec3(1);
-                vec3 color = vec3(0);
-
-                for (int i = 0; i < 8; ++i) {
-                    if (hit_spheres(r, hit)) {
-                        vec3 att = vec3(1);
-                        attenuation *= scatter(r, hit);
-                    }
-                    else {
-                        vec3 udir = normalize(r.dir);
-                        float t = .5 * (udir.y + 1.);
-                        color = mix(vec3(1), vec3(.5,.7,1), t);
-                        break;
-                    }
-                }
-
-                return color * attenuation;
-            }
-
-            void main() {
-                spheres[0] = sphere(vec3( 0, 0, 0), .5, tmaterial(0, vec3(.8, .3, .3), 0.));
-                spheres[1] = sphere(vec3(-1.1, 0, 0),                 .5, tmaterial(2, vec3(1., 1., 1.), 0.5));
-                spheres[2] = sphere(vec3( 1.1, 0, 0),                 .5, tmaterial(1, vec3(.8, .8, .8), 0.));
-                spheres[3] = sphere(vec3(0, -100.5, 0),           100., tmaterial(0, vec3(.8, .8,  0), 0.));
-
-                vec2 inv_size = 1. / u_viewport_size;
-                vec3 cam_pos = (u_view * vec4(0, 0, 0, 1)).xyz;
-                int passes  = int(u_rays_per_pixel);
-
-                float h = 0.5 / tan(radians(u_fov));
-
-                for (int i = 0; i < 128; ++i) {
-                    vec2 lens = rand_point_on_circle() * rand() * u_lens;
-                    vec3 lens_p = (u_view * vec4(lens, 0, 0)).xyz;
-                    vec4 dir = u_view * vec4(position.xy + randv2(.9) * inv_size, -h, 0);
-                    ray r = ray(cam_pos + lens_p, dir.xyz - lens_p);
-                    gl_FragColor.rgb += color(r);
-                    if (--passes == 0) {
-                        gl_FragColor.rgb /= u_rays_per_pixel;
-                        break;
-                    }
-                    if (u_frame > 1.) {
-                        break;
-                    }
-                }
-
-                if (u_frame > 1.) {
-                    gl_FragColor.rgb /= u_frame;
-                    gl_FragColor.rgb += (u_frame - 1.) * texture2D(u_prev, gl_FragCoord.xy/u_viewport_size).rgb / u_frame;
-                }
-
-                // gl_FragColor.rgb /= float(u_rays_per_pixel);
-                // gl_FragColor.rgb = sqrt(gl_FragColor.rgb);
-                // gl_FragColor.xyz += vec3(rand(), rand(), rand());
-                gl_FragColor.a = 1.;
-            }`);
+            }`, this.raytracer_frag_src.data);
         // use texture from register 0
         this.raytracer_shader.set_uniformi('u_prev', 0);
+        this.raytracer_shader.set_uniformi('u_spheres_tex', 1);
         const canvas = this.webgl_viewport.canvas;
         canvas.onmousemove = this.on_mouse_move.bind(this);
         canvas.onmousedown = this.on_mouse_down.bind(this);
@@ -5249,11 +5090,21 @@ class qc_app {
         document.onkeyup = this.on_key_up.bind(this);
         for (let attr of qu_attribute.g_list) {
             if (attr != this.frame_idx) {
-                attr.on_value_change_event.bind(() => this.frame_idx.value = 0);
+                attr.on_value_change_event.bind(() => {
+                    this.frame_idx.value = 0;
+                });
             }
         }
+        qu_attribute.on_any_attribute_change.bind(e => {
+            if (e !== this.frame_idx) {
+                this.frame_idx.value = 0;
+                this.render_sphere_data();
+            }
+        });
         this.on_resize();
         this.viewport_size.on_value_change_event.bind(this.on_resize.bind(this));
+        this.render_sphere_data();
+        this.start_loop();
     }
     on_mouse_up(ev) {
         this.mouse_down = false;
@@ -5352,22 +5203,38 @@ class qc_app {
         }
         requestAnimationFrame(this.update.bind(this));
     }
+    render_sphere_data() {
+        if (this.spheres_tex) {
+            this.spheres_tex.destroy(this.webgl_viewport.gl);
+        }
+        let data = [];
+        for (let sphere of this.spheres.value) {
+            data.push.apply(data, sphere.to_array());
+        }
+        let gl = this.webgl_viewport.gl;
+        this.raytracer_shader.set_uniformi('u_spheres_num', this.spheres.value.length);
+        this.spheres_tex = new qu_texture(this.webgl_viewport.gl, data.length / 4, 1, {
+            type: gl.FLOAT, format: 6408 /* RGBA */, wrap: 33071 /* CLAMP_TO_EDGE */, filter: 9728 /* NEAREST */, data
+        });
+    }
     render() {
         // this.canvas.clear();
         let prev = this.flip ? this.texture0 : this.texture1;
         let next = this.flip ? this.texture1 : this.texture0;
         this.flip = !this.flip;
         prev.bind();
+        if (this.spheres_tex) {
+            this.spheres_tex.bind(1);
+        }
         next.paint(() => {
             this.raytracer_shader.draw_mesh(this.quad, 'triangles');
         });
         next.bind();
         this.texture_shader.draw_mesh(this.quad, 'triangles');
     }
-    loop() {
+    start_loop() {
         requestAnimationFrame(this.update.bind(this));
     }
 }
 let app = new qc_app();
-app.init();
-app.loop();
+app.load();
---
+++ raytracer_frag.glsl
@@ -0,0 +1,241 @@
+#define FLT_MAX 3.402e+38
+#define PI 3.14159265359
+
+precision mediump float;
+
+varying vec4 position;
+
+uniform float u_frame;
+uniform float u_time;
+uniform float u_rays_per_pixel;
+uniform vec2  u_viewport_size;
+uniform float u_fov;
+uniform mat4  u_view;
+uniform float u_lens;
+uniform int  u_spheres_num;
+
+uniform sampler2D u_prev;
+uniform sampler2D u_spheres_tex;
+
+float g_rand_idx = 1.;
+float rand() {
+    const float a = 12.9898, b = 78.233, c = 43758.5453;
+    float dt = dot( fract(gl_FragCoord.xy * (g_rand_idx += .001) + (u_time/1000.)), vec2( a, b ) ), 
+    // highp float dt = dot( gl_FragCoord.xy, vec2( a, b ) ), 
+                sn = mod( dt, PI );
+    return fract(sin(sn) * c);
+}
+
+vec2 randv2(float length) {
+    return vec2(rand(), rand()) * 2. * length - length;
+}
+vec3 randv3(float length) {
+    return vec3(rand(), rand(), rand()) * 2. * length - length;
+}
+
+vec3 rand_point_in_sphere() {
+    // vec3 r = randv3(1.);
+    // return normalize(r) * pow(rand(), 1./3.);
+    return normalize(randv3(1.));
+    // return randv3(.5);
+}
+
+vec2 rand_point_on_circle() {
+    float alpha = rand() * PI;
+    return vec2(sin(alpha), cos(alpha));
+}
+
+struct ray { vec3 ori; vec3 dir; };
+
+vec3 ray_point(const ray r, float t) {
+    return r.ori + t * r.dir;
+}
+
+struct tmaterial {
+    int  type;
+    vec3 albedo;
+    float fuz; // for metal
+};
+
+struct hit_result {
+    float t; //time
+    vec3  p; //point
+    vec3  n; //normal
+    tmaterial m;
+};
+
+struct sphere {
+    vec3 center; 
+    float radius;
+    tmaterial material;
+};
+
+
+#define SPHERE_STRUCT_SIZE 6.0
+
+vec3 get_sphere_data(float idx, float prop) {
+    return texture2D(u_spheres_tex, vec2((idx * SPHERE_STRUCT_SIZE + prop + 0.5) / (float(u_spheres_num) * SPHERE_STRUCT_SIZE), 0)).rgb;
+}
+
+vec3 get_sphere_pos(float idx) {
+    return get_sphere_data(idx, 0.);
+}
+
+float get_sphere_radius(float idx) {
+    return get_sphere_data(idx, 1.).r;
+}
+
+tmaterial get_sphere_material(float idx) {
+    tmaterial m;
+    m.type   = int(get_sphere_data(idx, 2.).r);
+    m.albedo = get_sphere_data(idx, 3.);
+    m.fuz    = get_sphere_data(idx, 4.).r;
+    return m;
+}
+
+bool sphere_hit(float idx, const ray r, float tmin, float tmax, out hit_result hit) {
+    vec3 center = get_sphere_pos(idx);
+    float radius = get_sphere_radius(idx);
+    vec3 oc = r.ori - center;
+    float a = dot(r.dir, r.dir);
+    float b = 2. * dot(oc, r.dir);
+    float c = dot(oc, oc) - radius * radius;
+    float det = b*b - 4.*a*c; 
+
+    bool b_hit = false;
+    if (det > 0.) {
+        float tmp = (-b - sqrt(det))/(2.*a);
+
+        b_hit = tmp > tmin && tmp < tmax || 
+                (tmp = (-b + sqrt(det)) / (2.*a), tmp > tmin && tmp < tmax);
+
+        if (b_hit) {
+            hit.t = tmp;
+            hit.p = ray_point(r, tmp);
+            hit.n = (hit.p - center) / radius;
+            hit.m = get_sphere_material(idx);
+        }
+    }
+    return b_hit;
+}
+
+bool hit_spheres(const ray r, out hit_result res) {
+    hit_result hit;
+    bool b_hit = false;
+    float t_min = 0.001;
+    float t_max = FLT_MAX;
+
+    for (int i = 0; i < 128; ++i) {
+        if (i == u_spheres_num) {
+            break;
+        }
+        if (sphere_hit(float(i), r, t_min, t_max, hit)) {
+            t_max = hit.t;
+            res   = hit;
+            b_hit = true;
+        }
+    }
+
+    return b_hit;
+}
+
+float schlick(float cosine, float ref_idx) {
+    float r0 = (1. - ref_idx) / (1. + ref_idx);
+    r0 = r0 * r0;
+    return r0 + (1. - r0) * pow(1. - cosine, 5.);
+}
+
+vec3 scatter(inout ray r, const hit_result hit) {
+    // lambertian
+    if (hit.m.type == 0) {
+        vec3 target = hit.p + hit.n + rand_point_in_sphere();
+        r = ray(hit.p, target - hit.p);
+        return hit.m.albedo;
+    }
+    // metal
+    else if (hit.m.type == 1) {
+        vec3 t = reflect(normalize(r.dir), hit.n);
+        r = ray(hit.p, t + rand_point_in_sphere() * hit.m.fuz);
+        return hit.m.albedo;
+    }
+    // diaelectric (glass)
+    else if (hit.m.type == 2) {
+        vec3 n = hit.n;
+        const float ref_idx = 1.45;
+        float ni = 1. / ref_idx;
+        float hit_dot = dot(r.dir, hit.n);
+        float cosine = -hit_dot / length(r.dir);
+
+        if (hit_dot > 0.) {
+            n = -n;
+            ni = 1. / ni;
+            cosine *= -ref_idx;
+        }
+
+        vec3 refracted = refract(r.dir, n, ni);
+        if (dot(refracted, refracted) != 0.) {
+            if (rand() > schlick(cosine, ref_idx)) {
+                r = ray(hit.p, refracted);
+                return hit.m.albedo;
+            }
+        }
+
+        r = ray(hit.p, reflect(normalize(r.dir), hit.n));
+        return hit.m.albedo;
+    }
+    return vec3(0);
+}
+
+vec3 color(ray r) {
+    hit_result hit;
+    vec3 attenuation = vec3(1);
+    vec3 color = vec3(0);
+
+    for (int i = 0; i < 8; ++i) {
+        if (hit_spheres(r, hit)) {
+            attenuation *= scatter(r, hit);
+        }
+        else {
+            vec3 udir = normalize(r.dir);
+            float t = .5 * (udir.y + 1.);
+            color = mix(vec3(1), vec3(.5,.7,1), t);
+            break;
+        }
+    }
+
+    return color * attenuation;
+}
+
+void main() {
+    vec2 inv_size = 1. / u_viewport_size;
+    vec3 cam_pos = (u_view * vec4(0, 0, 0, 1)).xyz;
+    int passes  = int(u_rays_per_pixel);
+
+    float h = 0.5 / tan(radians(u_fov));
+
+    for (int i = 0; i < 128; ++i) {
+        vec2 lens = rand_point_on_circle() * rand() * u_lens;
+        vec3 lens_p = (u_view * vec4(lens, 0, 0)).xyz;
+        vec4 dir = u_view * vec4(position.xy + randv2(.9) * inv_size, -h, 0);
+        ray r = ray(cam_pos + lens_p, dir.xyz - lens_p);
+        gl_FragColor.rgb += color(r);
+        if (--passes == 0) {
+            gl_FragColor.rgb /= u_rays_per_pixel;
+            break;
+        }
+        if (u_frame > 1.) {
+            break;
+        }
+    }
+
+    gl_FragColor.rgb = sqrt(gl_FragColor.rgb);
+
+    if (u_frame > 1.) {
+        gl_FragColor.rgb /= u_frame;
+        gl_FragColor.rgb += (u_frame - 1.) * texture2D(u_prev, gl_FragCoord.xy/u_viewport_size).rgb / u_frame;
+    }
+    // if (gl_FragCoord.y < 10.)
+    // gl_FragColor.rgb = texture2D(u_spheres_tex, gl_FragCoord.xy/u_viewport_size).rgb;
+    // gl_FragColor.rg = gl_FragCoord.xy / u_viewport_size;
+    gl_FragColor.a = 1.;
+}
